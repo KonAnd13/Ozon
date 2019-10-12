e8541b3c488975b02720b11a7995cd15f6bd5427
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-10-12 23:51:34 +0300

    Update Main.java
    Добавление id

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -3,6 +3,7 @@ package ru.itpark;
 public class Main {
     public static void main(String[] args) {
         Lego lego_1 = new Lego(
+                143327311,
                 "https://www.ozon.ru/context/detail/id/143327311/",
                 "https://cdn1.ozone.ru/multimedia/c1200/1021657753.JPG",
                 "LEGO City Great Vehicles Дом на колесах Конструктор",
@@ -19,6 +20,7 @@ public class Main {
         );
 
         Lego lego_2 = new Lego(
+                154342351,
                 "https://www.ozon.ru/context/detail/id/154342351/",
                 "https://cdn1.ozone.ru/multimedia/c1200/1037592368.jpg",
                 "LEGO Hidden Side Трюковый грузовик Эль-Фуэго Конструктор",
@@ -35,6 +37,7 @@ public class Main {
         );
 
         Lego lego_3 = new Lego(
+                154342355,
                 "https://www.ozon.ru/context/detail/id/154342355/",
                 "https://cdn1.ozone.ru/multimedia/c1200/1037592160.jpg",
                 "LEGO Hidden Side Школа с привидениями Ньюбери Конструктор",
