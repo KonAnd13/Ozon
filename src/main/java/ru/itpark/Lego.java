307534d4aa361e0a630c88cb0ddfbd78a4872fa1
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-10-12 23:53:29 +0300

    Update Lego.java

--- src/main/java/ru/itpark/Lego.java
+++ src/main/java/ru/itpark/Lego.java
@@ -5,8 +5,8 @@ public class Lego extends Goods {
     private String series;
     private int childAge;
 
-    public Lego(String URL, String imageUrl, String name, int productCode, int price, double discount, String popularity, int rating, int reviews, int amountOfElements, String type, String series, int childAge) {
-        super(URL, imageUrl, name, productCode, price, discount, popularity, rating, reviews, amountOfElements);
+    public Lego(int id, String url, String imageUrl, String name, int productCode, int price, double discount, String popularity, int rating, int reviews, int amountOfElements, String type, String series, int childAge) {
+        super(id, url, imageUrl, name, productCode, price, discount, popularity, rating, reviews, amountOfElements);
         this.type = type;
         this.series = series;
         this.childAge = childAge;
